95ac2440b1a6b8f21e768b4acbf2c3c5b9138a89
Suraj Khatri <84454285+SurajCasey@users.noreply.github.com>
2025-07-14 23:47:00 +1000

    generateimage page

--- src/pages/GenerateImages.jsx
+++ src/pages/GenerateImages.jsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react'
 
 const GenerateImages = () => {
 
-  const imgStyle = [ 'Realistic', 'Ghibli style']
+  const imageStyle = [ 'Realistic', 'Ghibli style', 'Anime Style', 'Cartoon style', 'Fantasy style', 'Realistic style',
+    , '3D style', 'Potrait style'
+  ]
   
     const[selectedStyle, setSelectedStyle] = useState('Realistic')
     const[input, setInput] = useState('')
+    const[publish, setPublish] = useState(false)
   
     const onSubmitHandler = async (e) => {
       e.preventDefault();
@@ -17,35 +20,42 @@ const GenerateImages = () => {
       {/* Left Column */}
        <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
         <div className='flex items-center gap-3'>
-          <Sparkles className='w-6 text-[#6df768]'/>
+          <Sparkles className='w-6 text-[#00AD25]'/>
           <h1 className='text-xl font-semibold'>
             AI Image Generator
           </h1>
         </div>
         <p className='mt-6  text-sm font-medium'>Describe Your Image</p>
-        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+        <textarea onChange={(e) => setInput(e.target.value)} value={input} rows={4} 
+        className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
         placeholder='Describe what you want to see in the image.. ' required/>
 
         <p className='mt-4 text-sm font-medium'>Style</p>
 
         <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
-          {imgStyle.map((item, index)=>(
+          {imageStyle.map((item)=>(
             <span onClick={()=> setSelectedStyle(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer 
-              ${selectedStyle === item ? 'bg-purple-50 text-purple-700' : 'text-gray-500 border-gray-300'}`} 
+              ${selectedStyle === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`} 
             key={item}>{item}</span>
           ))}
         </div>
+        <div className='my-6 flex items-center gap-2'> 
+          <label className='relative cursor-pointer'>
+            <input type="checkbox" onChange={(e)=>setPublish(e.target.value)} checked={publish}
+            className='sr-only peer'/>
+          </label>
+        </div>
         <br />
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB]
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50]
         text-white px-4 py-2  mt-6 text-sm rounded-lg cursor-pointer'>
           <Image className='w-5'/>
-          Generate image
+          Generate Image
         </button>
       </form>
       {/* Right Column */}
         <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
           <div className='flex items-center gap-3'>
-            <Image className='w-5 h-5 text-[#8E37EB]'/>
+            <Image className='w-5 h-5 text-[#00AD25]'/>
             <h1 className='text-xl font-semibold'>Generated titles</h1>
           </div>
           <div className='flex-1 flex justify-center items-center'>
